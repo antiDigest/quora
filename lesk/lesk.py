2255abef53a7f3729ec69bc2e46428c35d4bf862
evamy <antriksh.don@gmail.com>
2017-04-16 00:30:10 -0500

    Adapted Lesk Finally complete

--- lesk/lesk.py
+++ lesk/lesk.py
@@ -2,7 +2,6 @@
 import nltk
 from nltk.corpus import wordnet as wn
 from nltk import word_tokenize
-from nltk.corpus.reader import NOUN
 
 import re
 import numpy as np
@@ -10,31 +9,21 @@ import numpy as np
 
 class Lesk(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, sentence):
+        self.sentence = sentence
+        self.meanings = {}
+        for word in word_tokenize(sentence):
+            self.meanings[word] = ''
 
-    def getMostFrequent(self, word):
-        senses = wn.synsets(word.lower())
-        # print word.lower(), senses
-        if senses == []:
-            return word.lower(), senses
-        maxValSense = len(senses[0].examples())
-        freqSense = senses[0]
-        for sense in senses:
-            # print len(sense.examples())
-            if len(sense.examples()) > maxValSense:
-                maxValSense = len(sense.examples())
-                freqSense = sense
+    def getSenses(self, word):
+        # print word
+        return wn.synsets(word.lower())
 
-        return freqSense, senses
-
-    def getAll(self, word):
-        senses = wn.synsets(word.lower())
-
-        if senses == []:
-            return word.lower(), senses
+    def getGloss(self, senses):
 
         gloss = {}
+
+        # print senses
 
         for sense in senses:
             gloss[sense.name()] = []
@@ -42,21 +31,15 @@ class Lesk(object):
         for sense in senses:
             gloss[sense.name()] += word_tokenize(sense.definition())
 
-        # print gloss
-
         return gloss
 
-    def computeOverlap(self, signature, context):
+    def getAll(self, word):
+        senses = self.getSenses(word)
 
-        # Base
-        overlap = 0
+        if senses == []:
+            return word.lower(), senses
 
-        # Step
-        for word in context:
-            if word in signature:
-                overlap += 1
-
-        return overlap
+        return self.getGloss(senses)
 
     def Score(self, set1, set2):
         # Base
@@ -72,11 +55,13 @@ class Lesk(object):
         return overlap
 
     def overlapScore(self, word1, word2):
-        nyms = ['hypernyms', 'hyponyms',
-                'member_meronyms', 'part_meronyms', 'substance_meronyms']
 
+        # print word1, word2
         gloss_set1 = self.getAll(word1)
-        gloss_set2 = self.getAll(word2)
+        if self.meanings[word2] == '':
+            gloss_set2 = self.getAll(word2)
+        else:
+            gloss_set2 = self.getGloss([wn.synset(self.meanings[word2])])
 
         score = {}
         for i in gloss_set1.keys():
@@ -91,61 +76,23 @@ class Lesk(object):
                 max_score = score[i]
                 bestSense = i
 
-        return bestSense, wn.synset(bestSense).definition()
+        return bestSense, max_score
 
-    def adaptedLesk(self, sentence):
+    def lesk(self, word, sentence):
         maxOverlap = 0
         context = word_tokenize(sentence.lower())
+        word_sense = []
+        for word_context in context:
+            # for sense in senses:
+            if not word == word_context:
+                self.meanings[word] = self.overlapScore(word, word_context)[0]
 
-        # index = context.index(word.lower())
-        # if index - k > 0 and index + k < len(context):
-        #     context = context[index - k: index + k]
-        # elif index + k < len(context):
-        #     context = context[0: index + k]
-        # elif index - k > 0:
-        #     context = context[index - k: len(context)]
-        # else:
-        #     context = context[0: len(context)]
+        return word, self.meanings[word], wn.synset(self.meanings[word]).definition()
 
-        print context
-        new_sent = []
-        for i, word in enumerate(context):
-            if i >= 0 and i < len(context) - 1:
-                new_sent.append(
-                    (word, self.overlapScore(word, context[i + 1])))
-            elif i == len(context) - 1:
-                new_sent.append(
-                    (word, self.overlapScore(word, context[i - 1])))
+# if __name__ == '__main__':
 
-                # for sense in senses:
+#     sentence = 'we take interest in him'
+#     sense = Lesk(sentence)
 
-        return new_sent
-
-    def simplifiedLesk(self, sentence, word):
-
-        bestSense, senses = self.getMostFrequent(word)
-        maxOverlap = 0
-        context = word_tokenize(sentence.lower())
-
-        if senses == []:
-            return 'ERROR: Cannot find a proper sense for the word !'
-
-        for sense in senses:
-            examples = [sense.definition()] + sense.examples()
-            signature = []
-            for example in examples:
-                signature += list(
-                    set(re.split('[^\w]* [^\w]*', example.lower())))
-            signature = list(set(signature))
-            # print signature
-            overlap = self.computeOverlap(signature, context)
-            if overlap > maxOverlap:
-                maxOverlap = overlap
-                bestSense = sense
-
-        return bestSense, bestSense.definition()
-
-if __name__ == '__main__':
-    sense = Lesk()
-
-    print sense.adaptedLesk('pine cone')
+#     for word in word_tokenize(sentence):
+#         print sense.lesk(word, sentence)
